2c6752235e05773654013312884544000194c631
joaki71bcn <joaquinagras@gmail.com>
2024-12-18 16:23:05 +0100

    python

--- PYTHON/reservas/reservas.py
+++ PYTHON/reservas/reservas.py
@@ -30,10 +30,24 @@ with sync_playwright() as p:
   p7_glorias_link = page.locator('a.parent:has(span:has-text("P7 Glòries"))')
   p7_glorias_link.hover()
   page.wait_for_selector('a[href="../Booking/Grid.aspx?id=8"]')
-  reserves_link = page.locator('a[href="../Booking/Grid.aspx?id=8"]').nth(0)  # Change 0 to the index of the desired element
+  reserves_link = page.locator('a[href="../Booking/Grid.aspx?id=8"]').nth(0)  # 0 PARA EL INDICE DE ELEMENTOS IGUALES, ME DABA ERROR PORQUE HABIA OTRO IGUAL
   reserves_link.click()
+  page.wait_for_timeout(4000) # SI NO PONIA ESTE TIMEOUT NO DABA TIEMPO A CARGAR EL CALENDARIO
 
-  # CALENDAR
+  # ABRE CALENDAR
+  date_calendar = page.locator('input#fechaTabla')
+  date_calendar.click()
+
+  # VARIABLES PARA SELECCIONAR UNA FECHA CONCRETA
+  target_day = "26"
+  target_month = "11"  # November (0-indexed, so 11 is December)
+  target_year = "2024"
+  # SELECTOR PARA LA FECHA
+  date_selector = f'td[data-month="{target_month}"][data-year="{target_year}"] a:has-text("{target_day}")'
+  # CLICK EN LA FECHA SELECCIONADA
+  page.locator(date_selector).click()
+  page.wait_for_timeout(4000)
+
   
 
 
