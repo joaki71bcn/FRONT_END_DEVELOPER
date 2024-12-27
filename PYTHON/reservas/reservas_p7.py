54ce1785bc065740d3ee4840c595eed0694f3084
joaki71bcn <joaquinagras@gmail.com>
2024-12-27 11:09:40 +0100

    peticion_email_habilitado_true

--- PYTHON/reservas/reservas_p7.py
+++ PYTHON/reservas/reservas_p7.py
@@ -3,26 +3,33 @@ from datetime import datetime, timedelta
 import time
 
 #
-################## SELECCION (VARIABLES) DIA Y HORA RESERVA    #######################
-target_day = "28"
+#
+# Para usar este programa cambiar los datos de la fecha y hora de reserva 
+# así como de la hora de lanzamiento en los 2 siguientes bloques
+#
+#
+#
+# Prompt the user for email and password
+email = input("Please enter your email: ")
+password = input("Please enter your password: ")
+#
+################## BLOQUE SELECCION (VARIABLES) DIA Y HORA RESERVA    #######################
+target_day = "30"
 target_month = "11"  # November (0-indexed, so 11 is December)
 target_year = "2024"
 print("Fecha: ",target_day, int(target_month) +1, target_year)
 print()
 #  HORA DEL PARTIDO "19:30-21:00"
-target_time = "10:00-11:30"
+target_time = "16:30-18:00"
 available_courts = []
 print("Hora: ", target_time)
 print()
-################## SELECCION (VARIABLES) DIA Y HORA RESERVA    #######################
 
-
-
-
+#
 ################    BLOQUE DE HORA PARA LANZAMIENTO DE APLICACION    ###################
 # Configuramos la hora objetivo para las 00:01 AM del día siguiente
 # comentar + timedelta si queremos hacerlo el mismo dia
-start_time = datetime.now().replace(hour=12, minute=56, second=0, microsecond=0) #+ timedelta(days=1)
+start_time = datetime.now().replace(hour=10, minute=25, second=0, microsecond=0) #+ timedelta(days=1)
 
 # OBTENER HORA ACTUAL
 current_time = datetime.now()
@@ -65,9 +72,10 @@ with sync_playwright() as p:
   accept_button = page.locator('input#ButtonPermitirNecesarios') 
   accept_button.wait_for(state='visible', timeout=4000) 
   accept_button.click() 
-
+  print("boton cookie ok")
   # ACCESS USUARI LINK PARA LOGIN ASPX
   page.click('a#linkUsuario') # hago click en el elemento a con id 
+  print("click en usuario ok")
 
   
 
@@ -75,13 +83,14 @@ with sync_playwright() as p:
   # EMAIL
   email_input = page.locator('input#ctl00_ContentPlaceHolderContenido_Login1_UserName')
   email_input.wait_for(state='visible', timeout=4000)
-  email_input.fill('@gmail.com')
+  email_input.fill(email)
   # PASSWORD
   pwd_input = page.locator('input#ctl00_ContentPlaceHolderContenido_Login1_Password')
-  pwd_input.fill('2022')
+  pwd_input.fill(password)
   # BOTON ACEPTAR
   form_button = page.locator('input#ctl00_ContentPlaceHolderContenido_Login1_LoginButton')
   form_button.click()
+  print("usuario y password ok")
   ####################### FORMULARIO LOGIN CREDENCIALES   #######################
 
 
@@ -93,11 +102,13 @@ with sync_playwright() as p:
   page.wait_for_selector('a[href="../Booking/Grid.aspx?id=8"]')
   reserves_link = page.locator('a[href="../Booking/Grid.aspx?id=8"]').nth(0)  # 0 PARA EL INDICE DE ELEMENTOS IGUALES, ME DABA ERROR PORQUE HABIA OTRO IGUAL
   reserves_link.click()
+  print("click en reservas ok")
   page.wait_for_timeout(4000) # SI NO PONIA ESTE TIMEOUT NO DABA TIEMPO A CARGAR EL CALENDARIO
 
   # ABRE CALENDAR
   date_calendar = page.locator('input#fechaTabla')
   date_calendar.click()
+  print("click en calandario ok")
   #
   #
   # SELECTOR PARA LA FECHA
@@ -106,6 +117,7 @@ with sync_playwright() as p:
 
   # CLICK EN LA FECHA SELECCIONADA
   page.locator(date_selector).click()
+  print("click en dia de calendario ok")
   page.wait_for_timeout(4000)
   
   # POSICIONES DE LAS 4 PRIMERAS PISTAS, ELEMENTO RECT X= DENTRO DE <G>
@@ -122,24 +134,28 @@ with sync_playwright() as p:
   reservation_made = False
   # BUCLE POR CADA PISTA [500,50,200,350]
   for x in court_x_positions:
-        button = court_slot.locator(f'rect[x="{x}"].buttonHora') 
+        button = court_slot.locator(f'rect[x="{x}"].buttonHora[habilitado="true"]') 
+        print("Para la pista",court_conversion[x],"hay:",button.count(),"libre")
         if button.count() > 0: # Verificamos si existe al menos un elemento
-          print("LOCATOR BOTONES RESERVA POR : ",x , button)
+          print("LOCATOR BOTONES RESERVA POR:",x , button)
           button.first.click() 
-          print("PISTA LIBRE: ", court_conversion[x])
+          print("Click ok en pista", court_conversion[x],"libre")
           # CLICK EN VENTANA DE EMERGENTE CON POLITICA DE RESERVA CHECKBOX
           checkbox = page.locator('input#terminos')
           checkbox.wait_for(state='visible', timeout=3000)
           checkbox.check()
+          print("Checkbox de reserva marcado")
           # CLICK EN EL BOTON 90 MINUTOS
           time_button = page.locator('.btnTiempo')
           time_button.click() 
+          print("Boton del tiempo clicado")
           page.wait_for_timeout(4000)
           # NUEVA VENTA PARA CONDICIONES LEGALES CHECKBOX
           checkbox_book = page.locator('input#ctl00_ContentPlaceHolderContenido_CheckBoxAceptoCondicionesLegales')
           # NO HA FUNCONADO CON WAIT_FOR, LO DEJO CON EL TIMEOUT DE ARRIBA
           # checkbox_book.wait_for(state='visible', timeout=4000) 
           checkbox_book.check()
+          print("Checkbox condiciones legales clicado")
           # BOTON CARREC CONTRA SALDO
           pay_button = page.locator('input#ctl00_ContentPlaceHolderContenido_ButtonPagoSaldo')
           pay_button.click()
